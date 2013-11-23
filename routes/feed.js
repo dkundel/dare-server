e12d3b271e619e2a9876f06071f2509c788831bd
Dominik Kundel <dominik.kundel@me.com>
2013-11-23 19:15:55 +0100

    sorted feed

--- routes/feed.js
+++ routes/feed.js
@@ -4,8 +4,19 @@ exports.latest = function (req, res, next) {
   var daresRef = db.child("dares");
 
   daresRef.once('value', function(data) {
+    data = data.val();
+    var array = _.map(data, function(value, key){
+      value.id = key;
+      return value;
+    });
+
+    array = _.sortBy(array, function(data) {
+      return (new Date(data.timestamp)).getTime();
+    });
+
+
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-    res.end(JSON.stringify(data.val()));
+    res.end(JSON.stringify(array));
     return next();
   });
 }
