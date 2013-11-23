3570bc21bc1d48841b6f23dd8150c95980afa52a
dmitryfd <fd@flexibleblog.ru>
2013-11-23 13:55:49 +0100

    updates...

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,22 @@
 // Create a new user, based on Fb auth data
 exports.create = function (req, res, next) {
   
-  var new_user = db.child("users").child("test");
+  var data = req.params;
+  var new_user = db.child("users").child(data.username);
 
   new_user.set({
-  	fb_id: "?", 
-  	fname: "?",
-  	lname: "?",
-  	imageurl: "?",
-  	email: "?",
-  	username: "?",
-  	score: 0,
-  	dares: []
+  	fb_id: data.fb_id, 
+  	fname: data.fname,
+  	lname: data.lname,
+  	imageurl: data.imageurl,
+  	email: data.email,
+  	username: data.username,
+  	score: 0
+  	//dares: [] -> empty arrays won't get written to Firebase
   });
 
-  res.send(req.params);
+  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+  res.end(JSON.stringify(data));
   return next();
 }
 
