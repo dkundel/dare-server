95e8d05d055d15598bd2b1b7e478299a92d7bb2c
dmitryfd <fd@flexibleblog.ru>
2013-11-23 12:38:19 +0100

    firebase test

--- server.js
+++ server.js
@@ -11,9 +11,19 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+var firebase = require('firebase');
+var db = new firebase(config.firebaseUrl);
+
 server.get('/echo/:name', function (req, res, next) {
   res.send(req.params);
   return next();
+});
+
+server.get('/dbtest', function  (req, res, next) {
+	/*db.child('users').child('vlad').set({name: "Vlad", age: 12});
+	db.child('users').on('value', function(snapshot) {
+  		console.log(snapshot.val());
+	});*/
 });
 
 server.listen(config.port, function () {
