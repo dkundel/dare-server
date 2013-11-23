01438bf182c91e17961ad501036e9accb13fc3c1
Dominik Kundel <dominik.kundel@me.com>
2013-11-23 12:24:32 +0100

    new config and initial server

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-// code!
+var restify = require('restify');
+var packageInfo = require('./package.json');
+var config = require('./config.json');
 
-console.log("Let's Start!");
+var server = restify.createServer({
+  name: packageInfo.name,
+  version: packageInfo.version
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/echo/:name', function (req, res, next) {
+  res.send(req.params);
+  return next();
+});
+
+server.listen(config.port, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
